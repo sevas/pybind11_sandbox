0cc8e3a9e4d185ebdb73154753437a3db51d6056
sevas <f.degroef@gmail.com>
2019-09-11 00:17:53 +0200

    add some more functions using eigen

--- python/hello.py
+++ python/hello.py
@@ -9,13 +9,39 @@ print(a)
 
 v_np = np.ones(shape=(200,), dtype=np.float32)
 print("sum(numpy): {}".format(mylib.sum(v_np)))
+print("sum_eigen(numpy): {}".format(mylib.sum_eigen(v_np)))
+
 
 v_l = [1.0, 1.0, 1.0, 1.0]
 print("sum(list): {}".format(mylib.sum(v_l)))
+print("sum_eigen(list): {}".format(mylib.sum_eigen(v_l)))
+
+a = np.array([
+    [120, 0,   320],
+    [  0, 120, 240],
+    [  0, 0, 1],
+])
+
+points = np.array([
+    [0, 0, 0], [0, 1, 0], [0, 0, 1]
+])
+
+res = mylib.mv_mul(a, points)
+
+print(res)
+
+mylib.array_add_scalar_stl(res.flatten(), 10)
+print(res)
+
+print(hex(res.ctypes.data))
+mylib.array_add_scalar_eigen(res, 10)
+print(res)
 
 
-# dm = mylib.DataManager()
 
-# print(dm.get_names())
 
-# print(mylib.get_vector_int(dm, "vector_int"))
+dm = mylib.DataManager()
+
+print(dm.get_names())
+
+print(mylib.get_vector_int(dm, "vector_int"))
--- src/lib/mylib.cpp
+++ src/lib/mylib.cpp
@@ -1,42 +1,65 @@
+#include <iostream>
+
+#include <functional>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#include "Eigen/Core"
+
+#include "pybind11/eigen.h"
+#include "pybind11/numpy.h"
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
-
-#include <vector>
-#include <string>
-#include <numeric>
-#include <functional>
-
 namespace py = pybind11;
 
+int add(int i, int j) { return i + j; }
 
-int add(int i, int j) {
-	return i + j;
-}
+std::vector<int> make_ints(const size_t size) { return std::vector<int>(size); }
 
+float sum(const std::vector<float>& values) { return std::accumulate(values.begin(), values.end(), 0.f, std::plus<float>()); }
 
-std::vector<int> make_ints(const size_t size)
+float sum_eigen(Eigen::MatrixXf& m)
 {
-	return std::vector<int>(size);
+    std::cout << m.rows() << "  " << m.cols() << std::endl;
+    return m.sum();
 }
 
+Eigen::MatrixX3f mv_mul(const Eigen::Matrix3f& M, const Eigen::MatrixX3f& points) { return M * points; }
 
-float sum(const std::vector<float>& values)
+void array_add_scalar_stl(std::vector<float>& in, const float val)
 {
-	return std::accumulate(values.begin(), values.end(), 0.f, std::plus<float>());
+    for (auto i = 0u; i < in.size(); ++i)
+    {
+        in[i] += val;
+    }
 }
 
+void array_add_scalar_eigen(Eigen::MatrixXf& in, const float val)
+{
+    std::cout << "ptr: " <<  &in << std::endl;
 
+	Eigen::MatrixXf values(in.rows(), in.cols());
+    values.setConstant(val);
+	in += values;
+}
 
 struct thing
 {
-	enum class elem_dtype { dt_void, dt_int, dt_float, dt_double, dt_char };
-	elem_dtype dtype;
-	void* data;
+    enum class elem_dtype
+    {
+        dt_void,
+        dt_int,
+        dt_float,
+        dt_double,
+        dt_char
+    };
+    elem_dtype dtype;
+    void* data;
 };
 
-
-// clang-format: off
+// clang-format off
 template<thing::elem_dtype dt> struct dtype_to_type{ typedef void elem_type; };
 template<> struct dtype_to_type<thing::elem_dtype::dt_int> { typedef int elem_type; };
 template<> struct dtype_to_type<thing::elem_dtype::dt_float> { typedef float elem_type; };
@@ -48,86 +71,71 @@ template<> struct type_to_dtype<int> { static const thing::elem_dtype dtype = th
 template<> struct type_to_dtype<float> { static const thing::elem_dtype dtype = thing::elem_dtype::dt_float; };
 template<> struct type_to_dtype<double> { static const thing::elem_dtype dtype = thing::elem_dtype::dt_double; };
 template<> struct type_to_dtype<char> { static const thing::elem_dtype dtype = thing::elem_dtype::dt_char; };
-
-
-// clang-format: on
+// clang-format on
 
 class data_manager
 {
 
+  public:
+    data_manager()
+    {
+        things["vector_int"] = thing{thing::elem_dtype::dt_int, (void*)new std::vector<int>(10)};
+        things["vector_float"] = thing{thing::elem_dtype::dt_float, (void*)new std::vector<float>(10)};
+        things["vector_double"] = thing{thing::elem_dtype::dt_double, (void*)new std::vector<double>(10)};
+    }
 
-public:
-	data_manager()
-	{
-		things["vector_int"] = thing{ thing::elem_dtype::dt_int, (void*)new std::vector<int>(10) };
-		things["vector_float"] = thing{ thing::elem_dtype::dt_float, (void*)new std::vector<float>(10) };
-		things["vector_double"] = thing{ thing::elem_dtype::dt_double, (void*)new std::vector<double>(10) };
+    ~data_manager() {}
 
-	}
+    template <typename T> std::vector<T>& get_data(const std::string& name)
+    {
+        auto item = things[name];
 
-	~data_manager()
-	{
-	}
+        typedef std::vector<T> ptr_type;
+        return *reinterpret_cast<ptr_type*>(item.data);
+    }
 
-	template<typename T>
-	std::vector<T>& get_data(const std::string& name)
-	{
-		auto item = things[name];
+    std::vector<std::string> get_names() const
+    {
+        std::vector<std::string> names;
+        for (auto& thing_entry : things)
+        {
+            names.push_back(thing_entry.first);
+        }
+        return names;
+    }
 
-		typedef std::vector<T> ptr_type;
-		return *reinterpret_cast<ptr_type*>(item.data);
-		
-	}
-
-	std::vector<std::string> get_names() const
-	{
-		std::vector<std::string> names;
-		for(auto &thing_entry : things)
-		{
-			names.push_back(thing_entry.first);
-		}
-		return names;
-	}
-	
-private:
-	std::map<std::string, thing> things;
+  private:
+    std::map<std::string, thing> things;
 };
 
+std::vector<int> get_vector_int(data_manager& dm, const std::string& name) { return dm.get_data<int>(name); }
 
-std::vector<int> get_vector_int(data_manager& dm, const std::string& name)
+std::vector<float> get_vector_float(data_manager& dm, const std::string& name) { return dm.get_data<float>(name); }
+
+std::vector<double> get_vector_double(data_manager& dm, const std::string& name) { return dm.get_data<double>(name); }
+
+PYBIND11_MODULE(mylib, m)
 {
-	return dm.get_data<int>(name);
-}
+    m.doc() = "pybind11 example plugin"; // optional module docstring
 
-std::vector<float> get_vector_float(data_manager& dm, const std::string& name)
-{
-	return dm.get_data<float>(name);
-}
+    m.def("add", &add, "A function which adds two numbers");
+    m.def("make_ints", &make_ints, "A function ");
+    m.def("sum", &sum, "Sums the elements in a vector");
+    m.def("sum_eigen", &sum_eigen, "Sums the elements in an arbitrary matrix");
 
-std::vector<double> get_vector_double(data_manager& dm, const std::string& name)
-{
-	return dm.get_data<double>(name);
-}
-
-
-
-
-
-PYBIND11_MODULE(mylib, m) {
-	m.doc() = "pybind11 example plugin"; // optional module docstring
-
-	m.def("add", &add, "A function which adds two numbers");
-	m.def("make_ints", &make_ints, "A function ");
-	m.def("sum", &sum, "Sums the elements in a vector");
+	m.def("array_add_scalar_stl", &array_add_scalar_stl, "Sums the elements in a vector");
+    m.def("array_add_scalar_eigen", &array_add_scalar_eigen, "Sums the elements in an arbitrary matrix");
 
 	
-	py::class_<data_manager>(m, "DataManager")
+    m.def("mv_mul", &mv_mul, "Mat x Vec product");
+
+    // clang-format off
+    py::class_<data_manager>(m, "DataManager")
 		.def(py::init())
 		.def("get_names", &data_manager::get_names);
+    // clang-format on
 
-	m.def("get_vector_int", &get_vector_int);
-	m.def("get_vector_float", &get_vector_float);
-	m.def("get_vector_double", &get_vector_double);
+    m.def("get_vector_int", &get_vector_int);
+    m.def("get_vector_float", &get_vector_float);
+    m.def("get_vector_double", &get_vector_double);
 }
-
-
